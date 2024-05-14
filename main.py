b0165da4a10a7542c11b1bc2e0afdc79d9823b90
Gv <ravigowthamaan@gmail.com>
2024-05-14 10:05:35 +0530

    test-run2:done

--- main.py
+++ main.py
@@ -2,25 +2,25 @@ from src.train import *
 from src.inference import *
 
 network_config = {
-    'embedding_size': 64,
-    'en_layers': 1,
-    'de_layers': 1,
-    'hidden_size': 16,
-    'cell': 'gru',
+    'embedding_size': 32,
+    'en_layers': 3,
+    'de_layers': 3,
+    'hidden_size': 1024,
+    'cell': 'lstm',
     'bidirectional': False,
-    'dropout': 0.0,
+    'dropout': 0.3,
     'beam_size': 5,
     'learning_rate': 1e-3,
-    'batch_size': 32,
-    'epochs': 30,
+    'batch_size': 256,
+    'epochs': 15,
     'lang': 'tam',
     'use_attention': False
 }
 
-# encoder, decoder, training_loss, validation_loss = train(network_config)
+encoder, decoder, training_loss, validation_loss = train(network_config)
 
-# torch.save(encoder, "model/outputs/encoder.h5")
-# torch.save(decoder, "model/outputs/decoder.h5")
+torch.save(encoder, "model/outputs/encoder.h5")
+torch.save(decoder, "model/outputs/decoder.h5")
 
 tensor, word, _ = inference("adangoiah", network_config['lang'])
 print(tensor)
